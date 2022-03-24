8c3e160e6eb0501b1cc8d5c3ce28b33b1a9a53d0
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-03-24 19:45:08 -0400

    to heroku

--- users_api/admin.py
+++ users_api/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import User, Home, Photo
-admin.site.register(User, Home, Photo)
+admin.site.register(User)
+admin.site.register(Home)
+admin.site.register(Photo)
