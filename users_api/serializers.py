ff0ce943ded086d54439a09dc383de82d02b17d6
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-03-24 19:19:18 -0400

    to heroku

--- users_api/serializers.py
+++ users_api/serializers.py
@@ -4,7 +4,7 @@ from .models import User, Home, Photo
 class UserSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = User # tell django which model to use
-        fields = ('id', 'name', 'password', 'email', 'firstName', 'lastName') # tell djan
+        fields = ('id', 'name', 'password', 'email', 'firstName', 'lastName',) # tell djan
 
 
 class HomeSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
