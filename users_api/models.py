cf674e1d2533fb2c812a930e9f2c9c63a0d9c8a4
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-03-24 18:25:15 -0400

    to heroku

--- users_api/models.py
+++ users_api/models.py
@@ -8,14 +8,15 @@ class User(models.Model):
     email= models.CharField(max_length=50)
 
 
+
 class Home(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.CharField(max_length=32)
     type = models.CharField(max_length=32)
     street = models.CharField(max_length=32)
     city = models.CharField(max_length=32)
     state = models.CharField(max_length=32)
 
+
 class Photo(models.Model):
-    home = models.ForeignKey(Home, on_delete=models.CASCADE)
+    home = models.CharField(max_length=32)
     photos = ArrayField(models.CharField(max_length=200, default='this', null=True), size=8)
- 
