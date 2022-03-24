6082db274f18301aaafa979685fa7247aa452bd0
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-03-24 16:56:37 -0400

    to heroku

--- users_api/models.py
+++ users_api/models.py
@@ -13,8 +13,4 @@ class Home(models.Model):
     street = models.CharField(max_length=32)
     city = models.CharField(max_length=32)
     state = models.CharField(max_length=32)
-    photos =  ArrayField(
-        ArrayField(
-            models.CharField(blank=True),
-            size=8,
-        ),
+    photos =  ArrayField(ArrayField(models.CharField(blank=True), size=8,),)
