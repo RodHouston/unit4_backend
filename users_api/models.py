b46741ffff869964b3042e2a8485fc6304434476
RodHouston <54164854+RodHouston@users.noreply.github.com>
2022-03-24 16:10:06 -0400

    to heroku

--- users_api/models.py
+++ users_api/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
 
 
 class Home(models.Model):
-    owner = models.CharField(max_length=32)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=32)
     street = models.CharField(max_length=32)
     city = models.CharField(max_length=32)
